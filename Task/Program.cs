441a6dd18b62662c57c852520f0a7baea656db6f
Sofya18 <119758211+Sofya18@users.noreply.github.com>
2022-12-03 23:58:50 +0300

    task №8

---
+++ Task/Program.cs
@@ -0,0 +1,11 @@
+﻿//task 8
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.Read());
+if (a % 2 == 0)
+{Console.WriteLine("Четное, делится без остатка");
+}
+else
+{Console.WriteLine("Нечетное, делится с остатком");
+}
